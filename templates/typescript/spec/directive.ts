a7f78c02fb5b0411a3724bd001959fb78184ad23
Jacob Eggers <jacob.eggers@wellsfargo.com>
2014-01-08 17:37:59 -0800

    Fixes based on comments by @jedmao

--- templates/typescript/spec/directive.ts
+++ templates/typescript/spec/directive.ts
@@ -1,6 +1,6 @@
-/// <reference path="../../../bower_components/DefinitivelyTyped/angularjs/angular.d.ts" />
-/// <reference path="../../../bower_components/DefinitivelyTyped/angularjs/angular-mocks.d.ts" />
-/// <reference path="../../../bower_components/DefinitivelyTyped/jasmine/jasmine.d.ts" />
+/// <reference path="../../../bower_components/dt-angular/angular.d.ts" />
+/// <reference path="../../../bower_components/dt-angular/angular-mocks.d.ts" />
+/// <reference path="../../../bower_components/dt-jasmine/jasmine.d.ts" />
 
 /// <reference path="../../../app/scripts/directives/<%= cameledName.toLowerCase() %>.ts" />
 
