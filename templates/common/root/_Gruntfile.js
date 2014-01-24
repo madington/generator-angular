233daed3e953ca279f676ae0decfb3eeeb644e46
Jacob Eggers <jacob.eggers@wellsfargo.com>
2014-01-23 18:39:16 -0800

    Recompile ngdocs when typescript has changed.

--- templates/common/root/_Gruntfile.js
+++ templates/common/root/_Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
       },<% } else if (typescript) { %>
       typescript: {
         files: ['<%%= yeoman.app %>/scripts/{,*/}*.ts'],
-        tasks: ['typescript:base'],
+        tasks: ['typescript:base', 'ngdocs'],
         options: {
           spawn: false
         }
